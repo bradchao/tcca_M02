0acf9317b1fd69392f9b21154b9ec990e13c8eff
Brad Chao <brad@brad.tw>
2017-10-22 16:05:22 +0800

    2017-10-22-V3

--- app/src/main/java/com/example/administrator/tcca_m02/MainActivity.java
+++ app/src/main/java/com/example/administrator/tcca_m02/MainActivity.java
@@ -4,12 +4,15 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private F1 f1;
     private F2 f2;
     private FragmentManager fmgr;
     private FragmentTransaction tran;
+    private boolean isF1;
 
 
     @Override
@@ -23,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
         tran = fmgr.beginTransaction();
         tran.add(R.id.container, f1);
         tran.commit();
-
+        isF1 = true;
     }
+
+    public void test(View view){
+        isF1 = !isF1;
+
+        tran = fmgr.beginTransaction();
+        tran.replace(R.id.container, isF1?f1:f2);
+        //tran.addToBackStack(null);
+        tran.commit();
+    }
+    public void test1(View view){
+        Log.i("brad", "Main");
+    }
+
 }
